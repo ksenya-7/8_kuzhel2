661b5dd0d64170d7f66e48d1d5852e516e855b54
Ksu <ksenya-7@yandex.ru>
2021-02-28 15:22:33 +0300

    внесены правки во все блоки

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -1,17 +1,43 @@
 const toggles = document.querySelectorAll('.main-footer__toggle.main-footer__toggle');
 const blocks = document.querySelectorAll('.main-footer__hide-block');
 
-const initAccordion = () => {
-  if (toggles && blocks) {
-    for (let i = 0; i < toggles.length; i++) {
-      toggles[i].classList.remove('main-footer__toggle--nojs');
+const closeLists = () => {
+  if (blocks) {
+    blocks.forEach((element) => {
+      element.classList.add('main-footer__hide-block--js');
+    });
+  }
+};
 
-      toggles[i].addEventListener('click', (evt) => {
-        evt.preventDefault();
-        blocks[i].classList.toggle('main-footer__hide-block--js');
-        toggles[i].classList.toggle('main-footer__toggle--active');
-      });
-    }
+const deactiveToggles = () => {
+  if (toggles) {
+    toggles.forEach((element) => {
+      element.classList.remove('main-footer__toggle--active');
+    });
+  }
+};
+
+const toggleBlock = (block, toggle) => {
+  block.classList.toggle('main-footer__hide-block--js');
+  toggle.classList.toggle('main-footer__toggle--active');
+};
+
+const initAccordion = () => {
+  closeLists();
+  deactiveToggles();
+  for (let i = 0; i < toggles.length; i++) {
+    toggles[i].classList.remove('main-footer__toggle--nojs');
+
+    toggles[i].addEventListener('click', (evt) => {
+      evt.preventDefault();
+      if (toggles[i].classList.contains('main-footer__toggle--active')) {
+        toggleBlock(blocks[i], toggles[i]);
+      } else {
+        closeLists();
+        deactiveToggles();
+        toggleBlock(blocks[i], toggles[i]);
+      }
+    });
   }
 };
 
